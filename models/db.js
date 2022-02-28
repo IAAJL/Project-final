9c7fd982debc174ceb3a084a69b1c4bc669a4799
IAAJL <98361270+IAAJL@users.noreply.github.com>
2022-03-01 03:55:14 +0530

    Add files via upload

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/TaskDB',{useNewUrlParser : true}, (err) =>
+mongoose.connect('mongodb+srv://test:test@project.hknbo.mongodb.net/Project?retryWrites=true&w=majority',{useNewUrlParser : true}, (err) =>
 {
     if(!err)
     console.log("Mongodb successully started")
